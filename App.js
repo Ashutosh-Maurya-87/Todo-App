5b297cfde6b02b4fbf5cbf0d73ddc9511d3f9818
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-07-11 21:47:59 +0530

    updste-app

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react/cjs/react.production.min';
 
 function App() {
+  const [change,setChange]=useState(
+    {
+      enter:" "
+    }
+  );
+setChange((preValue)=>{
+  console.log(preValue)
+})
+  const addEvent = (event) =>{
+
+    console.log(event.target.value)
+  }
   return(
     <>
     <h1> Todo List</h1>
-    <input type='text' />
-    <button>+</button>
+    <input
+    type='text'
+    placeholder='enter the item'
+    onChange={addEvent} 
+    vlaue={change.enter}
+    />
+    <button >+</button>
     </>
   )
   
