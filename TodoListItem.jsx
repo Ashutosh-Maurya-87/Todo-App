27a816b5e8ee5c2aca40ec5de7ef694382f6712c
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-07-18 18:51:33 +0530

    component-update-of-todo

--- TodoListItem.jsx
+++ TodoListItem.jsx
@@ -1,10 +1,38 @@
-import React  from 'react';
-
+import React, { useState }  from 'react';
+import './App.css';
 const TodoListItem = (props) =>{
+    const { text, index } = props
+    const [remove,setRemove]=useState('');
+    const removeFunc = () =>{
+        setRemove((text)=>{
+            
+            console.log('remove',text)
+        })
+
+    }
+    // Learning Object destructuring-------------------------------------
+    // const objects = {
+    //     fruits : {
+    //        apple:{
+    //         banana : {
+    //             lichi: '40 /kg',
+    //             guavava: '50 /kg'
+    //         }
+    //        }
+    //     }
+    // }
+    
+// console.log('object destruncting', objects && objects.fruits && objects.fruits.apple && objects.fruits.apple.banana  && objects.fruits.apple.banana.guavava)
+
+// console.log('object destruncting', objects?.fruits?.apple?.banana?.guavava)
+
+// const { fruits: {apple: { banana:{ lichi = ''} = {} } = {}} = {} } = objects;
+// console.log('destructuring', lichi)
     return(
     <>
-    <li key={props.index}><b> {props.text} </b>
+    <li key={index}><b> {text} </b>
     </li>
+    <button className ='removeBtn'onClick={removeFunc}>{remove}x</button>
     </>
     )
 }
