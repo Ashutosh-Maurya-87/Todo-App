d5ba52ae473ef0eaa3f7fbec4817a5657f0bc6b0
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-07-19 19:48:26 +0530

    component

--- TodoListItem.jsx
+++ TodoListItem.jsx
@@ -10,24 +10,7 @@ const TodoListItem = (props) =>{
         })
 
     }
-    // Learning Object destructuring-------------------------------------
-    // const objects = {
-    //     fruits : {
-    //        apple:{
-    //         banana : {
-    //             lichi: '40 /kg',
-    //             guavava: '50 /kg'
-    //         }
-    //        }
-    //     }
-    // }
     
-// console.log('object destruncting', objects && objects.fruits && objects.fruits.apple && objects.fruits.apple.banana  && objects.fruits.apple.banana.guavava)
-
-// console.log('object destruncting', objects?.fruits?.apple?.banana?.guavava)
-
-// const { fruits: {apple: { banana:{ lichi = ''} = {} } = {}} = {} } = objects;
-// console.log('destructuring', lichi)
     return(
     <>
     <li key={index}><b> {text} </b>
