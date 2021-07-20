d16bc39407b6fced7bc2f0501585f9d1c69d634b
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-07-20 11:07:34 +0530

    component

--- TodoListItem.jsx
+++ TodoListItem.jsx
@@ -1,15 +1,17 @@
 import React, { useState }  from 'react';
 import './App.css';
 const TodoListItem = (props) =>{
-    const { text, index,onSelect,rem } = props
+    const { text, id,onSelect,rem } = props
     
     
     console.log(props)
     return(
     <>
-    <li key={index}><b> {text} </b>
+    <li key={id}><b> {text} </b>
     </li>
-    <button className ='removeBtn'onClick={onSelect}>{rem}X</button>
+    <button className ='removeBtn'onClick={
+        onSelect(id)
+        }>{rem}X</button>
     </>
     )
 }
